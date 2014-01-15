d1efaf90b8f292a201174bc84269620e82b46eed
Roger Xu <roger.xu.2007@gmail.com>
2014-01-15 16:29:18 +0800

    Add angular ui libs.

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -1,4 +1,4 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui', 'ui.utils', 'ui.bootstrap']);
