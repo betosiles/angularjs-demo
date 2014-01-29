474c114ecbff971abf3770195fad7ed9298c3242
Roger Xu <roger.xu.2007@gmail.com>
2014-01-29 16:12:43 +0800

    Add boilerplate files.

--- src/main/webapp/scripts/config.js
+++ src/main/webapp/scripts/config.js
@@ -1,8 +1,40 @@
 'use strict';
 
+function getContextPath() {
+    var contextPath = "/" + window.location.pathname.split("/")[1]; // "/demo"
+    return contextPath;
+}
+
 var _config = {
-    contextPath: '.',
+    contextPath: getContextPath(),
     service: {
+        offline: true,
+        proxy: {
+            enabled: true,
+            path: 'proxy', // "http://localhost:8080/demo/proxy"
+        },
+        metadata: {
+            online: {
+                url: 'http://127.0.0.1:8080/demo/rest',
+                username: 'SYSTEM',
+                password: 'Abcd1234'
+            },
+            offline: {
+                url: 'data'
+            }
+        },
+        api: {
+            'items': {
+                online: {
+                    path: 'odata.xsodata/Items',
+                    type: 'odata'
+                },
+                offline: {
+                    path: 'items.json',
+                    type: 'json'
+                }
+            }
+        }
     },
     map: {
         location: {
@@ -14,3 +46,19 @@ var _config = {
 };
 
 app.constant('config', _config);
+
+// Configuring $translateProvider
+app.config(['$translateProvider', 'config', function($translateProvider, config) {
+    var i18nPath = config.contextPath + '/i18n';
+
+    // configures staticFilesLoader
+    $translateProvider.useStaticFilesLoader({
+        prefix: i18nPath + '/locale-',
+        suffix: '.json'
+    });
+
+    // load 'en' table on startup
+    $translateProvider.preferredLanguage('en');
+    $translateProvider.fallbackLanguage('en');
+    //$translateProvider.useLocalStorage();
+}]);
